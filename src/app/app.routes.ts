82c21d9a1f0565c242089bcf0fabaef9f55dd28f
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-11-24 11:35:17 +0100

    feat: add authentication and authorizations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,17 @@ import {ManageCategoryComponent} from './components/manage-category/manage-categ
 import {AuthGuard} from './security/auth-guard';
 import {LoginComponent} from './components/login/login.component';
 import {UnauthorizedComponent} from './components/unauthorized/unauthorized.component';
+import {HomeComponent} from './components/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'unauthorized', component: UnauthorizedComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductListComponent, canActivate: [AuthGuard], data: { roles: ['USER', 'ADMIN'] } },
   { path: 'products/:search', component: ProductListComponent, canActivate: [AuthGuard], data: { roles: ['USER', 'ADMIN'] } },
   { path: 'add-product', component: AddProductComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
   { path: 'update-product/:id', component: AddProductComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
-  { path: '', redirectTo: "products", pathMatch: "full" },
+  { path: '', redirectTo: "home", pathMatch: "full" },
   { path: 'categories', component: CategoryListComponent, canActivate: [AuthGuard], data: { roles: ['USER', 'ADMIN'] } },
   { path: 'add-category', component: ManageCategoryComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
   { path: 'update-category/:id', component: ManageCategoryComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
